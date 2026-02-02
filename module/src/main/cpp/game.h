7a2521a159e6f2851141292b65370ea393a1d41f
zhanqi1998 <yeniuzi222@gmail.com>
2026-02-02 16:22:43 +0800

    Change GamePackageName to 'com.tencent.rmcn'

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.ShuiSha.FPS2"
+#define GamePackageName "com.tencent.rmcn"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
